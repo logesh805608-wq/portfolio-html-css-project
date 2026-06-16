a753bd941b913feb30eacf307c96e93486b60f4d
Logesh <logesh805608@gmail.com>
2026-06-16 14:03:22 +0530

    Initial commit of portfolio files

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImg = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomDiceImg);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImg2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceImg2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 is winner";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 is winner";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
